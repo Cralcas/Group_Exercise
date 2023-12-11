c8c8d804fd7e8b6cf71c655549570277e5435c55
bea0f <beatricefriberg@hotmail.com>
2023-12-11 14:33:07 +0100

    Images done

--- src/ts/randomDogImage.ts
+++ src/ts/randomDogImage.ts
@@ -2,12 +2,11 @@ import { IRandom } from "./modules/IRandom";
 import { get } from "./serviceBase";
 
 
-const randomImagesContainer = document.getElementById("randomImages");
+const randomImagesContainer = document.getElementById("randomImage");
 
 export const randomDogImage = async (): Promise<string> => {
   const url = "https://dog.ceo/api/breeds/image/random";
   const data = await get<IRandom>(url);
-  console.log(data.message);
   createHtml(data.message);
   return data.message;
 };
@@ -16,10 +15,7 @@ const createHtml = (dog: string) => {
     let img = document.createElement("img");
     img.src = dog;
     img.alt = "bild på hund";
-    console.log(dog);
-    console.log("hej");
-    
-
+    img.className = "dog";
     randomImagesContainer.appendChild(img);
   }
 };
