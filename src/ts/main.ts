2a09047a5521e8b505ff13232bc7eeb8762df276
Ali Rezai <Ali.rezai27461@gmail.com>
2023-12-13 10:24:13 +0100

    !

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,11 @@
 import "./../scss/style.scss";
+
+const humBtn = document.getElementById("hum-btn")
+humBtn?.addEventListener("click", () => {
+    if(humBtn) {
+        const menu = document.getElementById("menu")
+        const menuList = document.createElement("div")
+        menuList.className = "test"
+        menu?.appendChild(menuList)
+    }
+})
